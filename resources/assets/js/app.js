2a9699d705b61bba434262cdb6c36588f146f355
AndresFelipeCano <andresfe98@gmail.com>
2018-05-11 10:38:07 -0500

    Se agregaron los cambios a la plantilla
    Se agragaron los estilos que la plantilla trae por defecto con sus
    funcionalidades, listas para implementar en nuestra app.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,16 +7,8 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+require('./vendor/chart.js/Chart.min.js');
+require('./vendor/datatables/jquery.dataTables.js');
+require('./sb-admin');
+require('./sb-admin-datatables');
+require('./sb-admin-charts');
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout.app');
 });
 Route::resource('equipo', 'EquiposController');
 Route::resource('usuario', 'UsuariosController');
